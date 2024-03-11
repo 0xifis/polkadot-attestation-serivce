085edc8859079dd5f9e79c0aea62d3beeadb5a27
Vishnu R Menon <vishthemenon@gmail.com>
2024-03-11 02:40:27 -0400

    Update index page

--- packages/nextjs/app/page.tsx
+++ packages/nextjs/app/page.tsx
@@ -1,67 +1,38 @@
 "use client";
 
-import Link from "next/link";
 import type { NextPage } from "next";
-import { useAccount } from "wagmi";
-import { BugAntIcon, MagnifyingGlassIcon } from "@heroicons/react/24/outline";
-import { Address } from "~~/components/scaffold-eth";
 
 const Home: NextPage = () => {
-  const { address: connectedAddress } = useAccount();
-
   return (
     <>
       <div className="flex items-center flex-col flex-grow pt-10">
         <div className="px-5">
           <h1 className="text-center">
             <span className="block text-2xl mb-2">Welcome to</span>
-            <span className="block text-4xl font-bold">Scaffold-ETH 2</span>
+            <span className="block text-4xl font-bold">Dolphin Protocol</span>
           </h1>
-          <div className="flex justify-center items-center space-x-2">
-            <p className="my-2 font-medium">Connected Address:</p>
-            <Address address={connectedAddress} />
-          </div>
-          <p className="text-center text-lg">
-            Get started by editing{" "}
-            <code className="italic bg-base-300 text-base font-bold max-w-full break-words break-all inline-block">
-              packages/nextjs/app/page.tsx
-            </code>
-          </p>
-          <p className="text-center text-lg">
-            Edit your smart contract{" "}
-            <code className="italic bg-base-300 text-base font-bold max-w-full break-words break-all inline-block">
-              YourContract.sol
-            </code>{" "}
-            in{" "}
-            <code className="italic bg-base-300 text-base font-bold max-w-full break-words break-all inline-block">
-              packages/hardhat/contracts
-            </code>
-          </p>
-        </div>
-
-        <div className="flex-grow bg-base-300 w-full mt-16 px-8 py-12">
-          <div className="flex justify-center items-center gap-12 flex-col sm:flex-row">
-            <div className="flex flex-col bg-base-100 px-10 py-10 text-center items-center max-w-xs rounded-3xl">
-              <BugAntIcon className="h-8 w-8 fill-secondary" />
-              <p>
-                Tinker with your smart contract using the{" "}
-                <Link href="/debug" passHref className="link">
-                  Debug Contracts
-                </Link>{" "}
-                tab.
-              </p>
-            </div>
-            <div className="flex flex-col bg-base-100 px-10 py-10 text-center items-center max-w-xs rounded-3xl">
-              <MagnifyingGlassIcon className="h-8 w-8 fill-secondary" />
-              <p>
-                Explore your local transactions with the{" "}
-                <Link href="/blockexplorer" passHref className="link">
-                  Block Explorer
-                </Link>{" "}
-                tab.
-              </p>
-            </div>
-          </div>
+          <article className="prose">
+            <hr />
+            <h2 className="text-center">The problem the Dolphin protocol solves</h2>
+            <p className="text-justify">
+              Tokenisation of Real World Assets is currently a 0/1 game: an Asset is either tokenized or not tokenised.
+              It is also centralised, slow, expensive and vendor-locked.
+              <br />
+              <br />
+              Here is an alternative process (general attestation service for the Polkadot ecosystem) that is
+              permissionless each step of the way and allows unprecedented granularity and flexibility. Anyone can start
+              from zero, claim that an NFT represents a RWA, and then slowly (or rapidly) convince others of it by
+              attaching Attestations to the NFT. The trustworthiness of the attestations is calculated by a
+              sophisticated real-time Social Graph using Pagerank and user-specific Priors (eg. Kilt ID, GitCoin
+              Passport, Harpie).
+              <br />
+              <br />
+              The marketplace for Attestations allows competitive forces to lower the prices of Attestations, and thus
+              allow even people who are poor to gradually tokenize an asset as their means allow. Even better, the whole
+              process being permissionless, you can donate to a stranger in need by paying for an attestation they
+              requested.
+            </p>
+          </article>
         </div>
       </div>
     </>
--- packages/nextjs/next.config.js
+++ packages/nextjs/next.config.js
@@ -9,6 +9,10 @@ const nextConfig = {
         protocol: "https",
         hostname: "**",
       },
+      {
+        protocol: "https",
+        hostname: "saxemberg.com",
+      },
     ],
   },
   typescript: {
